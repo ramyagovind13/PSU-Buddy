6a524f0d4c5d703b7c14035ccbb971758e030d8f
Ramya Govindaraj <ramyagovind13@gmail.com>
2024-03-05 15:31:08 -0800

    Added welcome message

--- src/app.py
+++ src/app.py
@@ -1,2 +1,11 @@
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
 
+# app config
+st.set_page_config(page_title="PSU Buddy", page_icon="🤖")
+st.title("PSU Buddy")
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I am a bot. How can I help you?"),
+    ]
